ac330d0f9131d7c4f89fc7f15f07e72d4f317803
delguoqing <delguoqing@hotmail.com>
2012-06-19 06:12:16 +0000

    * make_music_list.py pack tja_file, wave_file, title, subtitle, course, seek_pos for each course to generate a file called "fumen.lst"

--- psp-taikoclone/tools/make_music_list.py
+++ psp-taikoclone/tools/make_music_list.py
@@ -5,6 +5,7 @@ import optparse
 import os
 import os.path
 import sys
+import struct
 import unicodedata
 
 parser = optparse.OptionParser()
@@ -51,10 +52,11 @@ def get_tja_encoding(filename):
         return None
 
     encoding_list_valid_open = []
-    for encoding in encoding_list:        
-        wave_file = os.path.join(os.path.dirname(filename), header["WAVE"])
+    dirname = os.path.dirname(filename).decode("mbcs")
+    
+    for encoding in encoding_list:
         try:
-            wave_file = wave_file.decode(encoding)
+            wave_file = os.path.join(dirname, header["WAVE"].decode(encoding))
             assert isinstance(wave_file, unicode)
             if os.path.lexists(wave_file):
                 encoding_list_valid_open.append(encoding)
@@ -90,8 +92,8 @@ def get_tja_encoding(filename):
         return ret
                 
 def make_tja_info(tja_file, encoding):
-    info = {"tja_file": tja_file, "wave_file":"", "title":"", "subtitle":"",
-            "course_cnt":0, "course_levels":[], "seek_pos":[], ""}
+    info = {"tja_file": tja_file.decode(sys.getfilesystemencoding()), "wave_file":"", "title":"", "subtitle":"",
+            "course_cnt":0, "course_levels":[], "seek_pos":[], }
 
     f = None
     try:
@@ -129,6 +131,55 @@ def make_tja_info(tja_file, encoding):
 
     return info
 
+bad_data = "\0" * (200 * 2 + 100 * 2 + 1 + 8 * (4 + 4))
+    
+def pack_tja_info(info):
+
+    ret = ""
+    
+    # pack all data in utf8
+    
+    # limit filenames to 100
+    # padding if not enough
+    tja_file = info["tja_file"].encode("utf-8")
+    if len(tja_file) < 200:
+        tja_file += "\0" * (200 - len(tja_file))
+    else:
+        return bad_data
+        
+    wave_file = info["wave_file"].encode("utf-8")
+    if len(wave_file) < 200:
+        wave_file += "\0" * (200 - len(wave_file))
+    else:
+        return bad_data
+    
+    # limit title and subtitle to 50 
+    title = info["title"].encode("utf-8")
+    if len(title) < 100:
+        title += "\0" * (100 - len(title))
+    else:
+        title = title[:99] + "\0"
+        
+    subtitle = info["subtitle"].encode("utf-8")
+    if len(subtitle) < 100:
+        subtitle += "\0" * (100 - len(subtitle))
+    else:
+        subtitle = subtitle[:99] + "\0"    
+        
+    # limit to 8 course
+    ret += struct.pack(">200s200s100s100sB", tja_file, wave_file, title, subtitle, info["course_cnt"])
+    course_cnt = min(8, info["course_cnt"])
+    for i in xrange(8):
+        if i < course_cnt:
+            ret += struct.pack(">II", info["course_levels"][i], info["seek_pos"][i])
+        else:
+            ret += struct.pack(">II", 0, 0)
+    
+    assert len(ret) == 200 * 2 + 100 * 2 + 1 + 8 * (4 + 4), len(ret)
+    
+    return ret
+        
+    
 if __name__ == "__main__":
     options, args = parser.parse_args()
     
@@ -164,3 +215,20 @@ if __name__ == "__main__":
             print "Sorry,"    
             print "these files may contain some characters in them or filenames, python can't handle."
             print "Or, wave file is missing. Please check or rename"
+            
+    print "注意，会忽略路径名过长的文件"
+    
+#    max_str_len = 0
+#    for datum in data:
+#        max_str_len = max(max_str_len, len(datum["title"]))
+#        max_str_len = max(max_str_len, len(datum["subtitle"]))
+#        max_str_len = max(max_str_len, len(datum["tja_file"]))
+#        max_str_len = max(max_str_len, len(datum["wave_file"]))                
+#        print datum
+#    print max_str_len
+    f = open("fumen.lst", "wb")
+    f.write(struct.pack(">I", len(data)))
+    for datum in data:
+        binary_data = pack_tja_info(datum)
+        f.write(binary_data)
+    f.close()
