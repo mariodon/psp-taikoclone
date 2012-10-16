a6f683ee6dcc9e240a569a6714353caa13912dda
delguoqing <delguoqing@hotmail.com>
2012-10-16 11:40:26 +0000

    * add a function to print LWARC file list.

--- psp-taikoclone/rip_gim.py
+++ psp-taikoclone/rip_gim.py
@@ -1,9 +1,6 @@
 import struct
+import sys
 
-#1537_55 can not work right
-#365_13
-#356_3
-#356_4
 def rip(fname):
     f = open(fname, "rb")
     data = f.read()
@@ -27,6 +24,49 @@ def rip(fname):
 
     f.close()
 
-for i in xrange(1, 1540):
-    rip("part%d" % i)
-#rip("part1")
+def rip2(fname):
+	f = open(fname, "rb")
+	data = f.read()
+	f.close()
+	
+	if not data.startswith("LWARC"):
+		return
+	
+	off_filecnt = struct.unpack("<I", data[0xc:0x10])[0] + 0x8
+	off_filenameinfo = struct.unpack("<I", data[0x14:0x18])[0]
+	off_filesizeinfo = struct.unpack("<I", data[0x18:0x1c])[0]
+	off_filedata = struct.unpack("<I", data[0x1c:0x20])[0]
+	
+	filecnt = struct.unpack("<I", data[off_filecnt:off_filecnt+0x4])[0]
+	
+	cur_off_filenameinfo = off_filenameinfo
+	flist = []
+	max_fname_len = 0
+	for i in xrange(filecnt):
+		fname_len = struct.unpack("<I", 
+			data[cur_off_filenameinfo:cur_off_filenameinfo+0x4])[0]
+		fname = struct.unpack("<%ds" % fname_len, 
+			data[cur_off_filenameinfo+0x8:cur_off_filenameinfo+0x8+fname_len])[0]
+		fname = fname.rstrip('\x00')
+		
+		fsize = struct.unpack("<I",
+			data[off_filesizeinfo+i*0x8:off_filesizeinfo+i*0x8+0x4])[0]
+		foffset = struct.unpack("<I", 
+			data[off_filesizeinfo+i*0x8+0x4:off_filesizeinfo+i*0x8+0x8])[0]
+			
+		cur_off_filenameinfo += 0x8 + fname_len
+		
+		flist.append((fname, foffset+off_filedata, fsize))
+		max_fname_len = max(max_fname_len, len(fname))
+		
+	fmt_str = "filename: %%%ds" % max_fname_len + "\toff: 0x%x,\tsize: 0x%x"
+	
+	for finfo in flist:
+		print fmt_str % finfo
+	
+#for i in xrange(1, 1540):
+#    rip("part%d" % i)
+#rip2("E:\PSP\part10")
+
+if __name__ == "__main__":
+	rip2(sys.argv[1])
